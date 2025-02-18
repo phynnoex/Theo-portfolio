8867b435c05cc8268594ff258d3a90a71eb20874
Bankole Theophilus <71837809+phynnoex@users.noreply.github.com>
2025-02-17 23:31:45 -0500

    feat: update projects data; add new display images for Laundry-site and Pattern Library projects

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,4 +1,6 @@
 import projectDisplay from "../assets/projectDisplays/quizMobile.png";
+import laundaryDisplay from "../assets/projectDisplays/laundarySite.png";
+import patternLibraryDisplay from "../assets/projectDisplays/patternLibrary.png";
 
 
 const projects = [
@@ -13,34 +15,24 @@ const projects = [
         "displayImage": projectDisplay
     },
     {
-        "projectTitle": "E-Commerce",
-        "index": "2",
-        "AppType": "Web Application",
-        "stack": ["HTML", "JavaScript", "CSS"],
-        "projectDescription": "A fully responsive e-commerce website that allows users to browse products, add items to their cart, and complete purchases. The platform features an intuitive UI, interactive product displays, and a structured shopping experience. Designed with modern web technologies, the site ensures a smooth and engaging user experience.",
-        "githublink": "https://github.com/phynnoex/shopping-app",
-        "siteLink": "https://phynnoex.github.io/shopping-app",
-        "displayImage": projectDisplay
-    },
-    {
         "projectTitle": "Laundry-site Landing Page",
-        "index": "3",
+        "index": "2",
         "AppType": "Web Application",
         "stack": ["JavaScript", "HTML", "CSS"],
         "projectDescription": "A modern and clean landing page for a laundry service business. The site presents essential business details, including available services, pricing, and contact information. Designed with a user-friendly layout, the page uses subtle animations and responsive design to enhance customer engagement.",
         "githublink": "https://github.com/phynnoex/Laundry-site",
         "siteLink": "https://phynnoex.github.io/Laundry-site",
-        "displayImage": projectDisplay
+        "displayImage": laundaryDisplay
     },
     {
         "projectTitle": "Pattern Library/Dashboard",
-        "index": "4",
+        "index": "3",
         "AppType": "Web Application",
         "stack": ["HTML", "CSS", "D3.js", "Figma"],
         "projectDescription": "A comprehensive UI pattern library and interactive dashboard showcasing reusable design components and data visualizations. The project serves as a reference for maintaining consistent design patterns and UI elements in web applications. Built with D3.js, the dashboard includes dynamic charts and graphs for data representation, making it a useful tool for developers and designers.",
         "githublink": "https://github.com/phynnoex/patternlibrary",
         "siteLink": "https://phynnoex.github.io/patternlibrary",
-        "displayImage": projectDisplay
+        "displayImage": patternLibraryDisplay
     }
 
 ]
